82615fb91c1a7e7ba2a367dedfd2fb2ffe03e422
adoration29 <Adora Okogeri>
2019-03-10 15:07:02 -0400

    commit

---
+++ server.js
@@ -0,0 +1,48 @@
+var express = require('express');
+var bodyParser = require('body-parser');
+var mongoose = require('mongoose');
+
+var Article = require('./models/Article.js');
+
+var app = express();
+var PORT = process.env.PORT || 3000;
+
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({extended: true}));
+app.use(bodyParser.text());
+app.use(bodyParser.json({type:'application/vnd.api+json'}));
+
+app.use(express.static('./public'));
+
+var link = 
+//Local link
+// var link = 'mongodb://localhost/nytreact';
+
+mongoose.connect(link);
+var db = mongoose.connection;
+
+db.on('error', function (err) {
+  console.log('Mongoose Error: ', err);
+});
+
+db.once('open', function () {
+  console.log('Mongoose connection successful.');
+});
+
+app.get('/', function(req, res){
+  res.sendFile('./public/index.html');
+})
+
+app.get('/api/saved', function(req, res) {
+
+  Article.find({})
+    .exec(function(err, doc){
+
+      if(err){
+        console.log(err);
+      }
+      else {
+        res.send(doc);
+      }
+    })
+});
